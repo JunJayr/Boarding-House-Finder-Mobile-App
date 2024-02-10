3d5a90761d73214f930d3e613961b7c4c1b8b234
Gomersendo Godmalin <godmalin.gomersendojr03@gmail.com>
2024-02-10 11:21:52 +0800

    giutro

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tanle/Intro_page.dart';
-import 'package:tanle/bottom_navbar.dart';
 
 import 'firebase_options.dart';
 
@@ -21,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: NavBarPages(),
+      home: FirstPage(),
     );
   }
 }
